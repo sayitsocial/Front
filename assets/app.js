728840ad715f51eb47080c2770d0b7b349710f24
OjasFernandes <ojas0604@gmail.com>
2020-07-29 17:36:46 +0530

    check for errors

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+var nam= document.getElementById('name10');
+var eMa= document.getElementById('email10')
+var pas= document.getElementById('password10');
+var btn10= document.getElementById('button10');
+function btn()
+{
+console.log(nam,eMa,pas);
+}
+
+btn10.addEventListener('click',btn);
